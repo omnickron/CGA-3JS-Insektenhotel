b4b7b71a5a467357e0de92dc0302e733d6a6d4d5
omnickron <njschestag@gmail.com>
2023-02-01 18:53:04 +0100

    added grids

--- src/objects/FullHotel.js
+++ src/objects/FullHotel.js
@@ -14,6 +14,66 @@ export default class FullHotel extends THREE.Group {
     }
 
     addParts() {
+        let gitterMaterial = new THREE.LineBasicMaterial({color: 0x454545});
+        let untenGitterGeo = new THREE.PlaneGeometry(35.5, 20, 28, 15);
+        ToQuads(untenGitterGeo);
+        let gitter = new THREE.LineSegments(untenGitterGeo, gitterMaterial);
+        gitter.scale.set(0.85, 0.85, 0.85);
+        gitter.position.set(-7.7, -49.3, 17.35);
+        this.add(gitter);
+
+
+        let obenGitterGeo1 = new THREE.PlaneGeometry(20.2, 2.66, 16, 2);
+        ToQuads(obenGitterGeo1);
+        let obenGitter1 = new THREE.LineSegments(obenGitterGeo1, gitterMaterial);
+        obenGitter1.position.set(-7.7, -10, 18);
+        this.add(obenGitter1);
+        let obenGitterGeo2 = new THREE.PlaneGeometry(17.75, 2.66, 14, 2);
+        ToQuads(obenGitterGeo2);
+        let obenGitter2 = new THREE.LineSegments(obenGitterGeo2, gitterMaterial);
+        obenGitter2.position.set(-7.7, (2.66-10), 18);
+        this.add(obenGitter2);
+        let obenGitterGeo3 = new THREE.PlaneGeometry(12.67, 2.66, 10, 2);
+        ToQuads(obenGitterGeo3);
+        let obenGitter3 = new THREE.LineSegments(obenGitterGeo3, gitterMaterial);
+        obenGitter3.position.set(-7.7, (2.66+2.66-10), 18);
+        this.add(obenGitter3);
+        let obenGitterGeo4 = new THREE.PlaneGeometry(7.6, 2.66, 6, 2);
+        ToQuads(obenGitterGeo4);
+        let obenGitter4 = new THREE.LineSegments(obenGitterGeo4, gitterMaterial);
+        obenGitter4.position.set(-7.7, (2.66+2.66+2.66-10), 18);
+        this.add(obenGitter4);
+        let obenGitterGeo5 = new THREE.PlaneGeometry(2.53, 2.66, 2, 2);
+        ToQuads(obenGitterGeo5);
+        let obenGitter5 = new THREE.LineSegments(obenGitterGeo5, gitterMaterial);
+        obenGitter5.position.set(-7.7, (2.66+2.66+2.66+2.66-10), 18);
+        this.add(obenGitter5);
+
+
+        function ToQuads(g) {
+            let p = g.parameters;
+            let segmentsX = (g.type == "TorusBufferGeometry" ? p.tubularSegments : p.radialSegments) || p.widthSegments || p.thetaSegments || (p.points.length - 1) || 1;
+            let segmentsY = (g.type == "TorusBufferGeometry" ? p.radialSegments : p.tubularSegments) || p.heightSegments || p.phiSegments || p.segments || 1;
+            let indices = [];
+            for (let i = 0; i < segmentsY + 1; i++) {
+                let index11 = 0;
+                let index12 = 0;
+                for (let j = 0; j < segmentsX; j++) {
+                    index11 = (segmentsX + 1) * i + j;
+                    index12 = index11 + 1;
+                    let index21 = index11;
+                    let index22 = index11 + (segmentsX + 1);
+                    indices.push(index11, index12);
+                    if (index22 < ((segmentsX + 1) * (segmentsY + 1) - 1)) {
+                        indices.push(index21, index22);
+                    }
+                }
+                if ((index12 + segmentsX + 1) <= ((segmentsX + 1) * (segmentsY + 1) - 1)) {
+                    indices.push(index12, index12 + segmentsX + 1);
+                }
+            }
+            g.setIndex(indices);
+        }
         const hotelBody = new HotelBody();
         hotelBody.scale.set(0.85, 0.85, 0.85);
         hotelBody.updateMatrix();
